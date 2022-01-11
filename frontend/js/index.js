9a748b51899d80868157d61832525e2a6fdc0806
Anthony Audrain <anthonyaud78@gmail.com>
2022-01-11 16:55:44 +0100

    nouveau P7

---
+++ frontend/js/index.js
@@ -0,0 +1,7 @@
+const recupButton = document.querySelector(".button");
+recupButton.addEventListener("click", function(){
+    const recupAdresseMail = document.querySelector(".modifCompte");
+    const recupMdp = document.querySelector(".modifCompte2");
+    console.log(recupAdresseMail.value);
+    console.log(recupMdp.value);
+})
