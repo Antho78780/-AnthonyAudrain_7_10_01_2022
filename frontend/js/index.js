dc366da0050557b55bac46c0e0280ed61dff9d31
Anthony Audrain <anthonyaud78@gmail.com>
2022-01-12 03:07:23 +0100

    ajout de fichier HTML

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -2,6 +2,5 @@ const recupButton = document.querySelector(".button");
 recupButton.addEventListener("click", function(){
     const recupAdresseMail = document.querySelector(".modifCompte");
     const recupMdp = document.querySelector(".modifCompte2");
-    console.log(recupAdresseMail.value);
-    console.log(recupMdp.value);
+    window.location.href = "./profil.html"
 })
