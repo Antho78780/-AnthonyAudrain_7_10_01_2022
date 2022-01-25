e989012020cec23d77fffe7bca4f735daba8f99c
Anthony Audrain <anthonyaud78@gmail.com>
2022-01-25 02:07:51 +0100

    ajout de regex

--- backend/app.js
+++ backend/app.js
@@ -5,13 +5,5 @@ const app = express();
 
 app.use(express.json());
 
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb+srv://LABRADOR78:LABRADOR78@cluster0.qvmtp.mongodb.net/project2?retryWrites=true&w=majority",{
-useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log("connexion à mongoose réussi"))
-.catch(() => console.log("connexion à mongoose échoué"))
-
-
 
 module.exports = app;
--- frontend/js/accueil.js
+++ frontend/js/accueil.js
@@ -1,35 +1,73 @@
-const recupButton = document.querySelector(".modifButton");
+const recupButton = document.querySelector("#ensembleButton");
 
 if(recupButton) {
-    recupButton.addEventListener("click", function(){
-        const recupAdresseMail = document.querySelector(".modifAdresseMail");
-        const recupMdp = document.querySelector(".modifMdp");
-        if(recupAdresseMail.value == "" || recupMdp.value == "") {
+    recupButton[0].addEventListener("click", function(event){
+        event.preventDefault();
+        const formLogin = document.querySelector("#formLogin");
+        if(formLogin[0].value == "" || formLogin[1].value == "") {
             console.log("Aucun compte détecté")
         }
         else {
-            console.log(recupAdresseMail.value);
-            console.log(recupMdp.value);
+            console.log(formLogin[0].value);
+            console.log(formLogin[1].value);
             console.log("page suivante")
          
-            window.location.href="profil.html"
+            /**window.location.href="accueil2.html"**/
         }
     })
-    const creationCompte = document.querySelector(".inscription");
-    creationCompte.addEventListener("click", function() {
-        console.log(creationCompte);
+    recupButton[1].addEventListener("click", function() {
         window.location.href="inscription.html"
-        
     })
 }
 
-const informationRegister = document.querySelectorAll(".inscription2");
+const informationRegister = document.querySelector("#formInscription");
 const buttonSuivant = document.querySelector(".buttonSuivant");
+if(buttonSuivant) {
+    buttonSuivant.addEventListener("click", function(event) {
+        event.preventDefault();
+        const firstName = informationRegister[0].value;
+        const name = informationRegister[1].value;
+        const email = informationRegister[2].value;
+        const password = informationRegister[3].value; 
 
-for(let i=0;i<informationRegister.length;i++) {
-    buttonSuivant.addEventListener("click", function() {
-        console.log(informationRegister[i].value)
+        const tableauFormulaire = [firstName, name, email, password];
+        console.log(tableauFormulaire);
+
+        function validEmail() {
+            const verifEmail = email;
+            if(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(verifEmail)) {
+                return true;
+            }else {
+                return false;
+            };
+        }
+        function validPrenom() {
+            const verifPrenom = firstName;
+            if(/^[A-Za-z]{3,15}$/.test(verifPrenom)) {
+                return true;
+            }else {
+                return false;
+                
+            }; 
+        }
+        function validNom() {
+            const verifNom = name;
+            if(/^[A-Za-z]{3,15}$/.test(verifNom)) {
+                return true;
+            }else {
+                return false;
+                
+            }; 
+        }
+        if(validEmail() && validPrenom() && validNom()) {
+            console.log("page suivante");
+            window.location.href="accueil2.html"
+        }
+        else if (validEmail() == false || validPrenom() == false || validNom() == false){
+            alert("champs incorrects");
+        }
+
     })
-}
 
+}
 
